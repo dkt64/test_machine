699da914cb0e7ab0d0eb3644a30d56dfe666f9c3
dkt64 <b.apanasewicz@gmail.com>
2019-08-29 15:28:46 +0200

    no json

--- main.go
+++ main.go
@@ -7,49 +7,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
+	"time"
 )
 
-// Struktury
+// Machine - struktura maszyny
 // ============================================================================
-
-// SignalBool - zmienna TRUE/FALSE
-// Może być I/O lub flaga
-// ----------------------------------------------------------------------------
-type SignalBool struct {
-	ID  string
-	Val bool
-}
-
-// SignalInt - zmienna integer
-// Może być odczyt z czujnika analogowego
-// ----------------------------------------------------------------------------
-type SignalInt struct {
-	ID  string
-	Val int
-}
-
-// Signals - tablica zmiennych
-// ----------------------------------------------------------------------------
-type Signals struct {
-	SignalsBool []SignalBool
-	SignalsInt  []SignalInt
-}
-
-// Equipment - część maszyny, np. robot
-// Zawiera nazwę i stany sygnałów
-// ----------------------------------------------------------------------------
-type Equipment struct {
-	ID      string
-	Signals Signals
-}
-
-// Machine - zawiera equipmenty
-// ----------------------------------------------------------------------------
 type Machine struct {
-	ID        string
-	Equipment []Equipment
+	sName string
+	Robot struct {
+		sName          string
+		bHomePos       bool
+		bRobotMove     bool
+		bProgramActive bool
+	}
+	WeldingMachine struct {
+		sName               string
+		bValve              bool
+		bPosUp              bool
+		bPosDown            bool
+		bPreasureSensorBool bool
+		iCylinderPosition   int
+	}
+	Gripper struct {
+		sName     string
+		bValve    bool
+		bPosOpen  bool
+		bPosClose bool
+	}
 }
 
 // ErrCheck - obsługa błedów
@@ -60,41 +44,99 @@ func ErrCheck(errNr error) {
 	}
 }
 
+// ElapsedMilliseconds - sprawdznie odstepu czasu
+// ============================================================================
+func ElapsedMilliseconds(t0 int, czas int) bool {
+
+	t1 := time.Now().Nanosecond()
+
+	if ((t1 - t0) / 1000000) > czas {
+		return true
+	} else {
+		return false
+	}
+}
+
+// ProgSim - generowanie opóznień
+// ============================================================================
+func ProgSim(m *Machine) {
+
+	GripperValveTime0 := time.Now()
+
+	GripperValvePrev := m.Gripper.bValve
+
+	for {
+		t := time.Now()
+
+		// -------------------
+		// Symulacja grippera
+		// -------------------
+
+		// Gdy chcemy zamknąć pojawia się jedynka na zaworze
+		if !GripperValvePrev && m.Gripper.bValve {
+			GripperValveTime0 = t
+			m.Gripper.bPosClose = false
+			m.Gripper.bPosOpen = false
+		}
+
+		// Saprawdzmy czy minał czas
+		if ElapsedMilliseconds(GripperValveTime0.Nanosecond(), 5000) {
+			m.Gripper.bPosClose = true
+		}
+
+		GripperValvePrev = m.Gripper.bValve
+	}
+}
+
+// ProgInit - inicjacja zmiennych
+// ============================================================================
+func ProgInit(m *Machine) {
+	m.Gripper.bValve = false
+	m.Gripper.bPosClose = false
+	m.Gripper.bPosOpen = true
+
+	m.Robot.bHomePos = true
+	m.Robot.bProgramActive = true
+	m.Robot.bRobotMove = false
+
+	m.WeldingMachine.bPosDown = false
+	m.WeldingMachine.bPosUp = true
+	m.WeldingMachine.bPreasureSensorBool = false
+	m.WeldingMachine.bValve = false
+	m.WeldingMachine.iCylinderPosition = 100
+}
+
+// ProgRun - program realizowany przez maszynę (PLC+Robot)
+// ============================================================================
+func ProgRun(m *Machine) {
+
+	for {
+
+	}
+
+}
+
 // MAIN
 // ============================================================================
 func main() {
 
 	// Nasze zmienne
-	var machine Machine
-
-	// Otwieranie pliku JSON
-	jsonFile, err := os.Open("machine_def.json")
-	defer jsonFile.Close()
-	ErrCheck(err)
-
-	// Odczyt bajtów z pliku
-	byteStream, err := ioutil.ReadAll(jsonFile)
-	ErrCheck(err)
+	var m Machine
+	m.sName = "DTP testing machine"
+	m.Robot.sName = "KUKA"
+	m.Gripper.sName = "Festo"
+	m.WeldingMachine.sName = "Dalex"
+	ProgInit(&m)
 
 	// Wydruk struktury
-	fmt.Println("===================================================")
-	fmt.Println("Odczytane bajty...")
-	fmt.Println(string(byteStream))
-
-	// Parsowanie do obiektu
-	json.Unmarshal(byteStream, &machine)
-
-	// Wydruk struktury
-	fmt.Println("===================================================")
-	fmt.Println("Dostęp do struktury (ID)...")
-	fmt.Println(machine.ID)
-
-	// Wykonanie odwrotnej analizy
-	jsonStream, err := json.MarshalIndent(machine, "", "\t")
+	jsonStream, err := json.MarshalIndent(m, "", "\t")
 	ErrCheck(err)
-
-	// Wydruk struktury
 	fmt.Println("===================================================")
 	fmt.Println("Wygenerowana struktura...")
 	fmt.Println(string(jsonStream))
+
+	// Run machine program
+	fmt.Println("Start machine program... " + m.sName)
+	go ProgSim(&m)
+	ProgRun(&m)
 }
